aa170a98d2377ce7e58bdcfd5040479a6f46bcbe
lduchieu512 <lduchieu512@gmail.com>
2022-06-17 15:03:24 +0700

    video47

--- DACS3/DACS3/Appmanager/DACS3/app/src/main/java/com/manager/appbanhang/utils/Utils.java
+++ DACS3/DACS3/Appmanager/DACS3/app/src/main/java/com/manager/appbanhang/utils/Utils.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
-    public static final String BASE_URL="http://192.168.1.28:8080/banhang/";
+    public static final String BASE_URL="http://192.168.43.177:8080/banhang/";
     public static List<GioHang> manggiohang;
     public static List<GioHang> mangmuahang = new ArrayList<>();
     public static User user_current = new User();
